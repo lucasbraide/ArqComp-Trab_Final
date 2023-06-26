ecbb624f9d848059f48e41baf70ff0e5245720c7
marcelomx30 <marcelomeirelesmarquesfilho@hotmail.com>
2023-06-26 11:43:38 -0300

    alteracao factorialjr.asm

--- asm/factorialjr.asm
+++ asm/factorialjr.asm
@@ -6,6 +6,10 @@ c    ww 12
 u    ww 1     
 
 main add x, c
+    jz x, finalmain # Se N igual a 0, finaliza o programa e retorna r = 1       
+    sub x, u
+    jz x, finalmain # verificao se é 1
+    add x, u    
     mov x, r
     add y, r
     goto loop
@@ -16,3 +20,11 @@ loop sub x, u
     goto loop
 final mov y, r 
     halt 
+
+finalmain add y, r
+          jz y, caso1
+          mov y, r 
+          halt  
+caso1 add y, u
+      mov y, r
+      halt
